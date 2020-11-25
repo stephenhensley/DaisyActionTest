7f1a8037e49f37964c9d21873d4aa132f9b53547
stephenhensley <stephen.p.hensley@gmail.com>
2020-11-25 13:30:00 -0800

    intentionally bad style.

--- examples/passthru/passthru.cpp
+++ examples/passthru/passthru.cpp
@@ -12,12 +12,12 @@ int main(void)
 {
     bool state = false;
     hw.Configure();
-    hw.Init();
+        hw.Init();
     hw.StartAudio(passthru);
     for(;;)
     {
         hw.SetLed(state);
-        state = !state;
+    state = !state;
         dsy_system_delay(250);
     }
 }
