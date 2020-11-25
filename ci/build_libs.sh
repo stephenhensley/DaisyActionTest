810afffb01c6ae04a1dbfb398f6b4e82a6f3e5f8
stephenhensley <stephen.p.hensley@gmail.com>
2020-11-25 11:35:04 -0800

    added some echo to build libs.

--- ci/build_libs.sh
+++ ci/build_libs.sh
@@ -4,5 +4,11 @@ START_DIR=$PWD
 LIBDAISY_DIR=$PWD/libDaisy
 DAISYSP_DIR=$PWD/DaisySP
 
+echo "building libDaisy . . ."
 cd $LIBDAISY_DIR ; make clean ; make | grep "warning\|error" ;
+echo "done."
+
+echo "building DaisySP . . ."
 cd $DAISYSP_DIR ; make clean ; make | grep "warning\|error" ;
+echo "done."
+
