f5f15d3dd777a543f8427f68a762b837e9da7a54
stephenhensley <stephen.p.hensley@gmail.com>
2020-11-25 10:49:13 -0800

    added submodules and two simple examples.

---
+++ examples/blink/blink.cpp
@@ -0,0 +1,14 @@
+#include "daisy_seed.h"
+static daisy::DaisySeed hw;
+int main(void)
+{
+    bool state = false;
+    hw.Configure();
+    hw.Init();
+    for (;;)
+    {
+        hw.SetLed(state);
+        state = !state;
+        dsy_system_delay(125);
+    }
+}
---
+++ examples/passthru/passthru.cpp
@@ -0,0 +1,22 @@
+#include <cstring>
+#include "daisy_seed.h"
+
+static daisy::DaisySeed hw;
+
+void passthru(float **in, float**out, size_t size)
+{
+    memcpy(out, in, size * 2 * sizeof(in[0][0]));
+}
+
+int main(void)
+{
+    bool state = false;
+    hw.Configure();
+    hw.Init();
+    for (;;)
+    {
+        hw.SetLed(state);
+        state = !state;
+        dsy_system_delay(250);
+    }
+}
