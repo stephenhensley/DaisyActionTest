8c6a40821ee55148c426de2bea3b92b31f8c5065
stephenhensley <stephen.p.hensley@gmail.com>
2020-11-25 17:38:54 -0800

    better output filtering for buildlibs.

--- ci/build_libs.sh
+++ ci/build_libs.sh
@@ -5,10 +5,10 @@ LIBDAISY_DIR=$PWD/libDaisy
 DAISYSP_DIR=$PWD/DaisySP
 
 echo "building libDaisy . . ."
-cd $LIBDAISY_DIR ; make clean ; make | grep "warning\|error" ;
+cd $LIBDAISY_DIR ; make clean ; make | grep "warning:r\|error" ;
 echo "done."
 
 echo "building DaisySP . . ."
-cd $DAISYSP_DIR ; make clean ; make | grep "warning\|error" ;
+cd $DAISYSP_DIR ; make clean ; make | grep "warning:r\|error:" ;
 echo "done."
 
